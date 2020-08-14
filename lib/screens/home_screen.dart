d4871910502d4766c1c2db189ad536700bdaf864
abdimussa87@gmail.com <abdimussa87@gmail.com>
2020-08-14 08:13:33 +0300

    Done on Appbar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import '../config/palette.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            centerTitle: false,
+            title: Text(
+              'facebook',
+              style: const TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            actions: [
+              CircleActionButton(
+                icon: Icons.search,
+                iconSize: 30,
+                onPressed: () {},
+              ),
+              CircleActionButton(
+                icon: MdiIcons.facebookMessenger,
+                iconSize: 30,
+                onPressed: () {},
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
